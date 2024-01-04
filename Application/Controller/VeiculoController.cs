56f978bb5ee2cf07c35eb665affe7b90907f5de1
MARIA ESTELA PEREIRA GOMES BARBOSA <maria.epgbarbosa@senacsp.edu.br>
2024-01-03 22:14:56 -0300

    teste

---
+++ Application/Controller/VeiculoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Application.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VeiculoController : ControllerBase
+    {
+        [HttpPost("CadastrarVeiculo")]
+        public IActionResult PostAsync([FromBody] VeiculoCommand command)
+        {
+            return Ok();
+        }
+        public IActionResult SimularAluguel()
+
+        {
+            return Ok();
+        }
+        public IActionResult Alugar()
+        {
+            return Ok();
+        }   
+        
+    }
+}
---
+++ Domain/Enums/ETipoVeiculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum ETipoVeiculo
+    {
+        Esportivo = 1,
+        Hatch = 2,
+        SUV = 3,
+        Sedan = 4,
+
+
+    }
+}
